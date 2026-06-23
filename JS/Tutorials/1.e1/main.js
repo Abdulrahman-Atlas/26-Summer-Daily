16237a3405ce5bd6aeba35390b96f93e308fd794
ABDULRAHMAN JBAILI <abdulrahman.jbeyli@gmail.com>
2026-06-23 19:14:26 +0300

    getting familiar with the syntax

---
+++ JS/Tutorials/1.e1/main.js
@@ -0,0 +1,9 @@
+console.log("Sattof is GAY!");
+console.log("Cane Cegreme krdstani!");
+
+// window.alert("ALERT!");
+// window.alert("ALERT2!");
+// COMMENT
+
+document.getElementById("myH1").textContent = "Hello JS!";
+document.getElementById("myP").textContent = "This is a paragraph.";
---
+++ JS/Tutorials/2.Variables/index.js
@@ -0,0 +1,20 @@
+// Variables
+let age;
+age=25;
+console.log(`My age is ${age}.`);
+console.log(typeof age);
+
+let name = "ABDULRAHMAN";
+console.log(`My name is ${name}.`);
+console.log(typeof name);
+
+let online = true;
+console.log(`Am I online? ${online}.`);
+console.log(typeof online);
+
+let x = null;
+console.log(`The value of x is ${x}.`);
+console.log(typeof x);
+
+
+
