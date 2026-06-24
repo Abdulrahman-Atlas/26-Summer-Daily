41db4dffb8797a287b85fe126ebee1b3dc80a090
Abdulrahman-Atlas <abdulrahman.jbeyli@gmail.com>
2026-06-24 13:27:42 +0000

    Classic DVD idel screen

--- JS/gameDev-basics/1.Animating-2D/script.js
+++ JS/gameDev-basics/1.Animating-2D/script.js
@@ -6,17 +6,24 @@ const ctx = canvas.getContext('2d'); //context
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
-const playerImage = new Image();
-
-// playerImage.src = null;
 let x=0, y=0;
+let backNforth = true;
 function animate(){
     ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
     ctx.fillRect(x, y, 100, 100);
-    x++;
-    y++;
-    if(x > 600) x=0;
-    if(y > 600) y=0;
+    if(backNforth && x < 505){
+        x++;
+        y++;
+        if(x+1 >= 505) backNforth = !backNforth;
+    }
+
+    if(!backNforth && x > -1){
+        x--;
+        y--;
+        if(x-1 <= -1) backNforth = !backNforth
+    }
+    
     requestAnimationFrame(animate);
 };
 animate();
