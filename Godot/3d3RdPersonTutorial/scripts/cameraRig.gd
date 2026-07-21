21cf020320f30c735bf6d18b6a60adbaca33bd5d
Abdulrahman-Atlas <abdulrahman.jbeyli@gmail.com>
2026-07-21 18:26:35 +0300

    Added mouse movement controls

--- Godot/3d3RdPersonTutorial/scripts/cameraRig.gd
+++ Godot/3d3RdPersonTutorial/scripts/cameraRig.gd
@@ -1,13 +1,24 @@
 extends SpringArm3D
 
-var camera: Camera3D
-
+@onready  var camera: Camera3D = $Camera3D
+@export var sens : float = 100.0
+var mouseInput : Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	# overwriting spring length to match camera z position
 	camera = get_node("Camera3D")
 	spring_length = camera.position.z
+	# Mouse mode (lock mouse pointer)
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	mouseInput = mouseInput * sens * delta
+	rotation_degrees.y -= mouseInput.x
+	rotation_degrees.x -= mouseInput.y
+	rotation_degrees.x = clampf(rotation_degrees.x, -70, 50)
+	mouseInput = Vector2.ZERO
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		mouseInput = event.relative
